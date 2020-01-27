c33ac0e0838788b757906e2f1249288eba6cecdd
Jose Antonio Jijon <joseantoniojijon@Joses-MacBook-Pro.local>
2020-01-27 18:23:28 -0500

    Changes in EventRegistrationApplication file

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -1,18 +1,5 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class EventRegistrationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EventRegistrationApplication.class, args);
-	}
-
-}
-package ca.mcgill.ecse321.eventregistration;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
